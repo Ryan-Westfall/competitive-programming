bc5cfd006d42d40e4cffb0e796174d65e1df112b
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-04 20:38:59 -0700

    Backfill: 975-range-sum-of-bst - solution

---
+++ leetcode/975-range-sum-of-bst/solution.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+
+        total = 0
+        
+        def dfs(node):
+            nonlocal total
+            if not node:
+                return
+
+            if node.val >= low and node.val <= high:
+                total += node.val
+
+            if node.val < low:
+                dfs(node.right)
+                return
+
+            if node.val > high:
+                dfs(node.left)
+                return
+
+
+            dfs(node.right)
+            dfs(node.left)
+
+        dfs(root)
+
+        return total
+
+
+            

c67d08c43633adcbbb1944a5b2ae96c15e2d6966
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-04 20:49:04 -0700

    Backfill: 167-two-sum-ii-input-array-is-sorted - solution

---
+++ leetcode/167-two-sum-ii-input-array-is-sorted/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+
+        while l <= r:
+            if target > numbers[l] + numbers[r]:
+                l += 1
+            elif target < numbers[l] + numbers[r]:
+                r -= 1
+            elif target == numbers[l] + numbers[r]:
+                break
+
+        return [l + 1, r + 1]

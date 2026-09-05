41a1f3f8351155c1fa1f2850b942a8f44e3eb48a
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-04 20:20:14 -0700

    Backfill: 117-populating-next-right-pointers-in-each-node-ii - solution

---
+++ leetcode/117-populating-next-right-pointers-in-each-node-ii/solution.py
@@ -0,0 +1,28 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return
+        queue = deque([root])
+        while queue:
+            prev = None
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if prev:
+                    node.next = prev
+                prev = node
+                if node.right:
+                    queue.append(node.right)
+                if node.left:
+                    queue.append(node.left)
+
+        return root

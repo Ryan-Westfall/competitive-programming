ce69e71fd4cf8b02943f5a3a9d320ad3e9d5fb34
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-04 20:49:43 -0700

    Backfill: 1544-count-good-nodes-in-binary-tree - solution

---
+++ leetcode/1544-count-good-nodes-in-binary-tree/solution.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    good = 0
+    def goodNodes(self, root: TreeNode) -> int:
+
+        def dfs(curr, maxValue):
+            if not curr:
+                return None
+            maxValue = max(curr.val, maxValue)
+            if (curr.val >= maxValue):
+                self.good += 1
+            dfs(curr.left, maxValue)
+            dfs(curr.right, maxValue)
+
+        dfs(root, root.val)
+
+        return self.good
+        

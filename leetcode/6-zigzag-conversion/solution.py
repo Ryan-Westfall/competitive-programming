638f3f526d588c62c12cc36f49569a4fab49aeee
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-04 20:22:53 -0700

    Backfill: 6-zigzag-conversion - solution

---
+++ leetcode/6-zigzag-conversion/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def convert(self, s: str, numRows: int):
+        if numRows == 1:
+            return s
+
+        rows = [""] * numRows
+
+        row = 0
+        step = 1
+
+        for c in s:
+            rows[row] += c
+
+            if row == 0:
+                step = 1
+            elif row == numRows - 1:
+                step = -1
+
+            row += step
+
+        return "".join(rows)

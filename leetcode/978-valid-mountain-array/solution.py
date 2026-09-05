83bcf0fb2c69eaf6e78d065218b8ee45836a7b66
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-04 20:34:33 -0700

    Backfill: 978-valid-mountain-array - solution

---
+++ leetcode/978-valid-mountain-array/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        if len(arr) < 3:
+            return False
+
+        i = 0
+        while i + 1 < len(arr) and arr[i] < arr[i+1]:
+            i += 1
+
+        if i == len(arr) - 1 or i - 1 < 0:
+            return False
+
+        while i + 1 < len(arr) and arr[i] > arr[i+1]:
+            i += 1
+        
+        if i == len(arr) - 1:
+            return True
+        else:
+            return False

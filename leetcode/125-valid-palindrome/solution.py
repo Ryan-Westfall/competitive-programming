0112493825ed9b63459addece55f00f9120fe1e4
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-04 20:39:37 -0700

    Backfill: 125-valid-palindrome - solution

---
+++ leetcode/125-valid-palindrome/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+
+        while l < r:
+            # Move left pointer to the next alphanumeric character
+            while l < r and not s[l].isalnum():
+                l += 1
+            # Move right pointer to the next alphanumeric character
+            while l < r and not s[r].isalnum():
+                r -= 1
+
+            # Compare characters in a case-insensitive manner
+            if s[l].lower() != s[r].lower():
+                return False
+
+            l += 1
+            r -= 1
+
+        return True
+
+

f5f9ea8e73e6d2404937dad4375dc6f34c76e323
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-04 20:37:11 -0700

    Backfill: 1539-diagonal-traverse-ii - solution

---
+++ leetcode/1539-diagonal-traverse-ii/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
+
+        queue = collections.deque([[0,0]])
+        result = []
+
+        while queue:
+            row, col = queue.popleft()
+
+            result.append(nums[row][col])
+
+            if col == 0 and row + 1 < len(nums):
+                queue.append([row + 1, col])
+
+            if col + 1 < len(nums[row]):
+                queue.append([row, col + 1])
+
+        return result
+
+        

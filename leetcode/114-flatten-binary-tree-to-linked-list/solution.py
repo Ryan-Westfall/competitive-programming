ed5fc9d228a3d7aa03a747f6b965c09f3b593b04
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-04 20:21:02 -0700

    Backfill: 114-flatten-binary-tree-to-linked-list - solution

---
+++ leetcode/114-flatten-binary-tree-to-linked-list/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        prev = None
+
+        def dfs(node):
+            nonlocal prev
+
+            if not node:
+                return
+
+            dfs(node.right)
+            dfs(node.left)
+
+            node.right = prev
+            node.left = None
+
+            prev = node
+
+        dfs(root)

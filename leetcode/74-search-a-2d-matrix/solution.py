b5db9bf005676d2d9d34775ababe61b38ca434b0
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-06 21:53:08 -0700

    Add solution: 74-search-a-2d-matrix - solution

--- leetcode/74-search-a-2d-matrix/solution.py
+++ leetcode/74-search-a-2d-matrix/solution.py
@@ -1,37 +1,69 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        def inRow(index):
-            if target >= matrix[index][0] and target <= matrix[index][-1]:
-                return 0
-            elif target > matrix[index][-1]:
-                return 1
-            else: 
-                return -1
+        n = len(matrix)
+        m = len(matrix[0])
+        
+        rowLeft = 0
+        colLeft = 0
+        rowRight = n - 1
+        colRight = m - 1
 
-        def bs(row):
-            l,r = 0, len(row) - 1
-            while l <= r:
-                m = (l + r) // 2
-                if target > row[m]:
-                    l = m + 1
-                elif target < row[m]:
-                    r = m - 1
-                else:
-                    return True
-            return False
+        while rowLeft <= rowRight or colLeft <= colRight:
+            midRow = (rowRight+rowLeft) // 2
+            midCol = (colRight+colLeft) // 2
 
-        rowL, rowR = 0, len(matrix) - 1
-        while rowL <= rowR:
-            rowM = (rowL + rowR) // 2
-            print(rowM)
-            cal = inRow(rowM)
-            print(cal)
-            if cal == 0:
-                return bs(matrix[rowM])
-            elif cal == 1:
-                rowL = rowM + 1
-            else:
-                rowR = rowM - 1
+            print(rowRight, colRight, rowLeft, colLeft, 'midRow', midRow, 'midCol', midCol, matrix[midRow][midCol])
+
+            if target == matrix[midRow][midCol]:
+                return True
+            elif target < matrix[midRow][midCol]:
+                print('cat')
+                tempRowLeft = 0
+                tempRowRight = midRow - 1
+                while tempRowLeft <= tempRowRight:
+                    tempRowMid = (tempRowLeft + tempRowRight) // 2
+                    if matrix[tempRowMid][midCol] == target:
+                        return True
+                    elif target > matrix[tempRowMid][midCol]:
+                        tempRowLeft = tempRowMid + 1
+                    elif target < matrix[tempRowMid][midCol]:
+                        tempRowRight = tempRowMid - 1
+                tempColLeft = 0
+                tempColRight = midCol - 1
+                while tempColLeft <= tempColRight:
+                    tempColMid = (tempColLeft + tempColRight) // 2
+                    if matrix[midRow][tempColMid] == target:
+                        return True
+                    elif target > matrix[midRow][tempColMid]:
+                        tempColLeft = tempColMid + 1
+                    elif target < matrix[midRow][tempColMid]:
+                        tempColRight = tempColMid - 1
+                rowRight = midRow - 1
+                colRight = midCol - 1
+            elif target > matrix[midRow][midCol]:
+                tempRowLeft = midRow + 1
+                tempRowRight = n - 1
+                while tempRowLeft <= tempRowRight:
+                    tempRowMid = (tempRowLeft + tempRowRight) // 2
+                    if matrix[tempRowMid][midCol] == target:
+                        return True
+                    elif target > matrix[tempRowMid][midCol]:
+                        tempRowLeft = tempRowMid + 1
+                    elif target < matrix[tempRowMid][midCol]:
+                        tempRowRight = tempRowMid - 1
+                tempColLeft = midCol + 1
+                tempColRight = m - 1
+                while tempColLeft <= tempColRight:
+                    tempColMid = (tempColLeft + tempColRight) // 2
+                    if matrix[midRow][tempColMid] == target:
+                        return True
+                    elif target > matrix[midRow][tempColMid]:
+                        tempColLeft = tempColMid + 1
+                    elif target < matrix[midRow][tempColMid]:
+                        tempColRight = tempColMid - 1
+                print('hi')
+                rowLeft = midRow + 1
+                colLeft = midCol + 1
+            
 
         return False
-

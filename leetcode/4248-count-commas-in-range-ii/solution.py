f83e94688119880b0b19c2db79d2e583a42370b0
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-07 20:41:04 -0700

    Add solution: 4248-count-commas-in-range-ii - solution

--- leetcode/4248-count-commas-in-range-ii/solution.py
+++ leetcode/4248-count-commas-in-range-ii/solution.py
@@ -1,14 +1,12 @@
 class Solution:
     def countCommas(self, n: int) -> int:
-        nLen = len(str(n))
-        total = ((nLen-1) // 3) * n
-        cur = 1
-        prev = 0
-        minusCount = (nLen-1) // 3
-        while cur < n:
-            cur *= 1000
-            total -= ((cur-1) - prev) * minusCount
-            minusCount -= 1
-            prev = cur - 1
+        total = 0
+        power = 1000
+        commas = 1
+
+        while power <= n:
+            total += n - power + 1
+            power *= 1000
+            commas += 1
 
         return total

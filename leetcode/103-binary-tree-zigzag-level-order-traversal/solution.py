4cb39a765d8099cd820889d94de3c3865d5a7129
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-09 17:18:09 -0700

    Add solution: 103-binary-tree-zigzag-level-order-traversal - solution

--- leetcode/103-binary-tree-zigzag-level-order-traversal/solution.py
+++ leetcode/103-binary-tree-zigzag-level-order-traversal/solution.py
@@ -1,28 +1,34 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
-        
+
         queue = deque([root])
-        flipped = False
+        left_to_right = True
         output = []
+
         while queue:
             cur = []
-            for _ in range(len(queue)):
-                node = queue.pop()
-                cur.append(node.val)
 
-                if node.left:
-                    queue.appendleft(node.left)
-                if node.right:
-                    queue.appendleft(node.right)
-            output.append(cur if not flipped else cur[::-1])
-            flipped = not flipped
+            for _ in range(len(queue)):
+                if left_to_right:
+                    node = queue.popleft()
+                    cur.append(node.val)
+
+                    if node.left:
+                        queue.append(node.left)
+                    if node.right:
+                        queue.append(node.right)
+                else:
+                    node = queue.pop()
+                    cur.append(node.val)
+
+                    if node.right:
+                        queue.appendleft(node.right)
+                    if node.left:
+                        queue.appendleft(node.left)
+
+            output.append(cur)
+            left_to_right = not left_to_right
 
         return output

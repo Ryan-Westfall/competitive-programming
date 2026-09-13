df2be2665830e269a974d49dde74dd43d24263e6
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-12 17:55:55 -0700

    Add solution: 864-image-overlap - solution

--- leetcode/864-image-overlap/solution.py
+++ leetcode/864-image-overlap/solution.py
@@ -2,28 +2,21 @@ class Solution:
     def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
         n = len(img1)
 
-        img1List = []
-        img2Set = set()
+        ones1 = []
+        ones2 = []
+
         for r in range(n):
             for c in range(n):
-                if img2[r][c] == 1:
-                    img2Set.add((r,c))
-                if img1[r][c] == 1:
-                    img1List.append((r,c))
+                if img1[r][c]:
+                    ones1.append((r, c))
+                if img2[r][c]:
+                    ones2.append((r, c))
 
-        maxCount = 0
+        shifts = defaultdict(int)
 
-        coordinates = [(i, j) for i in range(-n, n + 1) for j in range(-n, n + 1)]
+        for r1, c1 in ones1:
+            for r2, c2 in ones2:
+                shift = (r2 - r1, c2 - c1)
+                shifts[shift] += 1
 
-        for dr, dc in coordinates:
-            count = 0
-            for r, c in img1List:
-                if (r+dr,c+dc) in img2Set:
-                    count += 1
-
-            maxCount = max(maxCount, count)
-
-        return maxCount
-
-
-            
+        return max(shifts.values(), default=0)

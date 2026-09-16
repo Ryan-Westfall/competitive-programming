c09df022aa8d4ab5664f1c27a97a10a5bd29e575
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-16 14:16:13 -0700

    Add solution: 494-target-sum - solution

--- leetcode/494-target-sum/solution.py
+++ leetcode/494-target-sum/solution.py
@@ -2,20 +2,20 @@ from collections import defaultdict
 
 class Solution:
     def findTargetSumWays(self, nums: list[int], target: int) -> int:
+        n = len(nums)
 
-        dp = defaultdict(int)
-        dp[0] = 1
+        dp = [defaultdict(int) for _ in range(n)]
 
-        for num in nums:
-            next_dp = defaultdict(int)
+        # First number
+        dp[0][nums[0]] += 1
+        dp[0][-nums[0]] += 1
 
-            for curSum, ways in dp.items():
-                next_dp[curSum + num] += ways
-                next_dp[curSum - num] += ways
+        for i in range(1, n):
+            for curSum, ways in dp[i - 1].items():
+                dp[i][curSum + nums[i]] += ways
+                dp[i][curSum - nums[i]] += ways
 
-            dp = next_dp
-
-        return dp[target]
+        return dp[n - 1][target]
 
         # @cache
         # def dp(i, curSum):

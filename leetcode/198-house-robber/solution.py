4f0fb61d3f9fa91b2fa435e10373edeff98e505a
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-16 14:50:12 -0700

    Add solution: 198-house-robber - solution

--- leetcode/198-house-robber/solution.py
+++ leetcode/198-house-robber/solution.py
@@ -1,17 +1,26 @@
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        self.memo = {}
-        
-        def dfs(index):
-            if index >= len(nums):
+    def rob(self, nums: list[int]) -> int:
+        # n = len(nums)
+        # dp = [0] * n
+        # dp[0] = nums[0]
+
+        # for i in range(1,n):
+        #     # Take + 2
+        #     dp[i] += max(dp[i-1], nums[i+1])
+        #     # NoTake + 1
+        #     dp[i]
+
+        # return dp[n-1]
+
+        @cache
+        def dp(i):
+            if i >= len(nums):
                 return 0
 
-            if (index) in self.memo:
-                return self.memo[(index)]
+            take = nums[i] + dp(i+2)
+            noTake = dp(i+1)
 
-            skip = dfs(index + 1)
-            rob =  nums[index] + dfs(index + 2)
-            self.memo[(index)] = max(skip,rob)
-            return max(skip,rob)
+            return max(take, noTake)
+            
 
-        return dfs(0)
+        return dp(0)

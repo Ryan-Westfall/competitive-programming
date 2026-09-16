25e292102d73d646eff124afdad1eb24229107b2
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-16 14:52:44 -0700

    Add solution: 198-house-robber - solution

--- leetcode/198-house-robber/solution.py
+++ leetcode/198-house-robber/solution.py
@@ -1,26 +1,29 @@
 class Solution:
     def rob(self, nums: list[int]) -> int:
-        # n = len(nums)
-        # dp = [0] * n
-        # dp[0] = nums[0]
+        n = len(nums)
+        dp = [0] * n
+        dp[0] = nums[0]
 
-        # for i in range(1,n):
-        #     # Take + 2
-        #     dp[i] += max(dp[i-1], nums[i+1])
-        #     # NoTake + 1
-        #     dp[i]
+        for i in range(1,n):
+            # Take + 2
+            take = nums[i]
+            if i >= 2:
+                take += dp[i-2]
+            # NoTake + 1
+            noTake = dp[i-1]
+            dp[i] = max(take, noTake)
 
-        # return dp[n-1]
+        return dp[n-1]
 
-        @cache
-        def dp(i):
-            if i >= len(nums):
-                return 0
+        # @cache
+        # def dp(i):
+        #     if i >= len(nums):
+        #         return 0
 
-            take = nums[i] + dp(i+2)
-            noTake = dp(i+1)
+        #     take = nums[i] + dp(i+2)
+        #     noTake = dp(i+1)
 
-            return max(take, noTake)
+        #     return max(take, noTake)
             
 
-        return dp(0)
+        # return dp(0)

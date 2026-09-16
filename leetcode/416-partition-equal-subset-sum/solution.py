55e75b9ecce79b04bd2b8920b8c4e5a0c6ffe302
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-16 15:35:22 -0700

    Add solution: 416-partition-equal-subset-sum - solution

--- leetcode/416-partition-equal-subset-sum/solution.py
+++ leetcode/416-partition-equal-subset-sum/solution.py
@@ -3,44 +3,54 @@ class Solution:
         total = sum(nums)
         n = len(nums)
 
-        # dp = [[0] * total for _ in range(n)]
-        # dp[0][nums[0]] = 1
-        # dp[0][0] = 1
-
-        # for i in range(1,n):
-        #     cur = nums[i]
-        #     for amount in range(total):
-
-        #         # NoTake
-        #         dp[i][amount] = dp[i-1][amount]
-
-        #         # Take
-        #         if 
-        #         dp[i][curTotal] += dp[]
-
-
-        # return len(dp[n-1][total//2])
-
-        n = len(nums)
-        total = sum(nums)
-
         if total % 2:
             return False
 
         target = total // 2
 
-        @cache
-        def dp(i, cur):
-            if cur > target:
-                return False
+        dp = [[False] * (target + 1) for _ in range(n)]
 
-            if i == n:
-                return cur == target
+        dp[0][0] = True
+        if nums[0] <= target:
+            dp[0][nums[0]] = True
 
-            return (
-                dp(i + 1, cur) or
-                dp(i + 1, cur + nums[i])
-            )
+        for i in range(1, n):
+            cur = nums[i]
 
-        return dp(0, 0)
+            for amount in range(target + 1):
+
+                # NoTake
+                dp[i][amount] = dp[i - 1][amount]
+
+                # Take
+                if amount >= cur:
+                    dp[i][amount] = (
+                        dp[i][amount] or
+                        dp[i - 1][amount - cur]
+                    )
+
+        return dp[n - 1][target]
+
+        # n = len(nums)
+        # total = sum(nums)
+
+        # if total % 2:
+        #     return False
+
+        # target = total // 2
+
+        # @cache
+        # def dp(i, cur):
+        #     if cur > target:
+        #         return False
+
+        #     if i == n:
+        #         return cur == target
+
+        #     return (
+        #         dp(i + 1, cur) or
+        #         dp(i + 1, cur + nums[i])
+        #     )
+
+        # return dp(0, 0)
 

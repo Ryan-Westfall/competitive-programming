d894a6ef94ec6b643cbc653ebbc6a13c0c391dbd
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-16 12:35:30 -0700

    Add solution: 70-climbing-stairs - solution

--- leetcode/70-climbing-stairs/solution.py
+++ leetcode/70-climbing-stairs/solution.py
@@ -1,13 +1,26 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n <= 2:
-            return n
+        
+        # def dp(x):
+        #     if x == 1:
+        #         return 1
+        #     if x == 2:
+        #         return 2
+
+
+        #     return dp(x- 1) + dp(x-2)
+
+        # return dp(n)
+
 
         dp = [0] * (n + 1)
-        dp[1] = 1
-        dp[2] = 2
+        for i in range(1, n+1):
+            if i == 1:
+                dp[1] = 1
+            elif i == 2:
+                dp[2] = 2
+            else:  
+                dp[i] = dp[i-1] + dp[i-2]
 
-        for i in range(3, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
 
         return dp[n]

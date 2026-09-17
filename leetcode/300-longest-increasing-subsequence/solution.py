e4705e456b03cbce4cce992760021c6b66ccf447
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-17 14:32:30 -0700

    Add solution: 300-longest-increasing-subsequence - solution

--- leetcode/300-longest-increasing-subsequence/solution.py
+++ leetcode/300-longest-increasing-subsequence/solution.py
@@ -2,16 +2,27 @@ class Solution:
     def lengthOfLIS(self, nums: list[int]) -> int:
         n = len(nums)
 
-        @cache
-        def dp(i):
-            if i == n:
-                return 0
+        # @cache
+        # def dp(i):
+        #     if i == n:
+        #         return 0
 
+        #     maxTake = 1
+        #     for j in range(i+1, n):
+        #         if nums[i] < nums[j]:
+        #             maxTake = max(maxTake, dp(j) + 1)
+
+        #     return maxTake
+
+        # return max([dp(i) for i in range(n)])
+
+        dp = [0] * n
+        for i in range(n):
             maxTake = 1
-            for j in range(i+1, n):
-                if nums[i] < nums[j]:
-                    maxTake = max(maxTake, dp(j) + 1)
+            for j in range(i-1, -1, -1):
+                if nums[i] > nums[j]:
+                    maxTake = max(maxTake, dp[j] + 1)
 
-            return maxTake
+            dp[i] = maxTake
 
-        return max([dp(i) for i in range(n)])
+        return max(dp)

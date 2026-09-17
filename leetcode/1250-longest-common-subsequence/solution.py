2bf879859be9fccb5c1beafaada1e9c3134cf5de
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-17 16:27:26 -0700

    Add solution: 1250-longest-common-subsequence - solution

--- leetcode/1250-longest-common-subsequence/solution.py
+++ leetcode/1250-longest-common-subsequence/solution.py
@@ -8,10 +8,9 @@ class Solution:
             if i == iLen or j == jLen:
                 return 0
 
-            take = 0
             if text1[i] == text2[j]:
-                take = dp(i+1, j+1) + 1
+                return dp(i+1, j+1) + 1
             
-            return max(take, dp(i+1, j), dp(i, j+1))
+            return max(dp(i+1, j), dp(i, j+1))
 
         return dp(0,0)

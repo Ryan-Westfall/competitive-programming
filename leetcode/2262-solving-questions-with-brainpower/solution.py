0f38a91343307b61915d79475da061ef4de69db7
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-17 13:44:13 -0700

    Add solution: 2262-solving-questions-with-brainpower - solution

--- leetcode/2262-solving-questions-with-brainpower/solution.py
+++ leetcode/2262-solving-questions-with-brainpower/solution.py
@@ -2,16 +2,33 @@ class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
         
         n = len(questions)
-        @cache
-        def dp(i):
-            if i >= n:
-                return 0
+        # @cache
+        # def dp(i):
+        #     if i >= n:
+        #         return 0
 
 
-            noTake = dp(i+1)
+        #     noTake = dp(i+1)
+        #     point, brainpower = questions[i]
+        #     take = dp(i+brainpower+1) + point
+
+        #     return max(take, noTake)
+
+        # return dp(0)
+
+
+        dp = [0] * (n + 1)
+
+        for i in range(n):
             point, brainpower = questions[i]
-            take = dp(i+brainpower+1) + point
 
-            return max(take, noTake)
+            # Skip
+            dp[i + 1] = max(dp[i + 1], dp[i])
 
-        return dp(0)
+            # Take
+            next_i = min(n, i + brainpower + 1)
+            dp[next_i] = max(dp[next_i], dp[i] + point)
+
+        return dp[n]
+            
+

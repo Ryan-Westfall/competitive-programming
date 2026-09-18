3cd676156065705ae39fa1d3d76cbd19ed8cebea
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-17 17:03:19 -0700

    Add solution: 1250-longest-common-subsequence - solution

--- leetcode/1250-longest-common-subsequence/solution.py
+++ leetcode/1250-longest-common-subsequence/solution.py
@@ -15,13 +15,18 @@ class Solution:
 
         # return dp(0,0)
 
-        dp = [[0] * (jLen + 1) for _ in range(iLen + 1)]
-
+        dp = [0] * (jLen + 1)
         for i in range(1, iLen + 1):
-            for j in range(1, jLen + 1):
-                if text1[i-1] == text2[j-1]:
-                    dp[i][j] = dp[i-1][j-1] + 1
-                else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            prev = 0
 
-        return dp[iLen][jLen]
+            for j in range(1, jLen + 1):
+                old = dp[j]       # dp[i-1][j]
+
+                if text1[i-1] == text2[j-1]:
+                    dp[j] = prev + 1
+                else:
+                    dp[j] = max(dp[j], dp[j-1])
+
+                prev = old        # becomes dp[i-1][j] for next iteration
+
+        return dp[jLen]

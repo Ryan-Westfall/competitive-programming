f97010a3ccf81f4fad82188e7f5163fb0615905d
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-17 17:28:57 -0700

    Add solution: 72-edit-distance - solution

--- leetcode/72-edit-distance/solution.py
+++ leetcode/72-edit-distance/solution.py
@@ -1,17 +1,27 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        cache = [[float("inf")] * (len(word2) + 1) for i in range(len(word1) + 1)]
-        
-        for j in range(len(word2) + 1):
-            cache[len(word1)][j] = len(word2) - j
-        for i in range(len(word1) + 1):
-            cache[i][len(word2)] = len(word1) - i
-            
-        for i in range(len(word1) - 1, -1, -1):
-            for j in range(len(word2) - 1, -1, -1):
-                if word1[i] == word2[j]:
-                    cache[i][j] = cache[i+1][j+1]
-                else:
-                    cache[i][j] = 1 + min(cache[i+1][j], cache[i][j+1], cache[i+1][j+1])
-                    
-        return cache[0][0]
+        n = len(word1)
+        m = len(word2)
+
+        @cache
+        def dp(i, j):
+            if i == n and j == m:
+                return 0
+
+            if i == n:
+                return m - j
+
+            if j == m:
+                return n - i
+
+            if word1[i] == word2[j]:
+                return dp(i+1, j+1)
+
+            return min(
+                dp(i+1, j) + 1,       # delete
+                dp(i, j+1) + 1,       # insert
+                dp(i+1, j+1) + 1      # replace
+            )
+
+
+        return dp(0,0)

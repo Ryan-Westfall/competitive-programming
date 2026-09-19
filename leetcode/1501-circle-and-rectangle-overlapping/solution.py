255b250e12817f0168c9b78d37899ddf2502d705
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-18 21:57:09 -0700

    Add solution: 1501-circle-and-rectangle-overlapping - solution

--- leetcode/1501-circle-and-rectangle-overlapping/solution.py
+++ leetcode/1501-circle-and-rectangle-overlapping/solution.py
@@ -1,18 +1,11 @@
 class Solution:
-    def checkOverlap(self, radius: int, xCenter: int, yCenter: int, x1: int, y1: int, x2: int, y2: int) -> bool:
-        if x1 <= xCenter <= x2 and y1 <= yCenter <= y2:
-            return True
-            
-        closest = float('inf')
-        startX, startY, endX, endY = x1, y1, x2, y2
-        for x in range(startX, endX+1):
-            startYEuclidianDistance = ((xCenter - x)**2 + (yCenter - startY) ** 2) ** (1/2)
-            endYEuclidianDistance = ((xCenter - x)**2 + (yCenter - endY) ** 2) ** (1/2)
-            closest = min(closest, startYEuclidianDistance, endYEuclidianDistance)        
+    def checkOverlap(self, radius: int, xCenter: int, yCenter: int,
+                     x1: int, y1: int, x2: int, y2: int) -> bool:
 
-        for y in range(startY, endY+1):
-            startXEuclidianDistance = ((xCenter - startX)**2 + (yCenter - y) ** 2)**(1/2)
-            endXEuclidianDistance = ((xCenter - endX)**2 + (yCenter - y) ** 2)**(1/2)
-            closest = min(closest, startXEuclidianDistance, endXEuclidianDistance)
-            
-        return closest <= radius
+        closestX = max(x1, min(xCenter, x2))
+        closestY = max(y1, min(yCenter, y2))
+
+        return (
+            (xCenter - closestX) ** 2 +
+            (yCenter - closestY) ** 2
+        ) <= radius ** 2

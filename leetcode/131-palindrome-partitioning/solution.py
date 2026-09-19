a4145c088a91fb65d015b0b4df5d799361c17a81
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-18 17:16:22 -0700

    Add solution: 131-palindrome-partitioning - solution

--- leetcode/131-palindrome-partitioning/solution.py
+++ leetcode/131-palindrome-partitioning/solution.py
@@ -2,22 +2,19 @@ class Solution:
     def partition(self, s: str) -> list[list[str]]:
         n = len(s)
         ans = []
-        def backtracking(i, j, cur):
-            if j == n or i == n:
-                if "".join(cur) == s:
-                    ans.append(cur[:])
+
+        def backtracking(i, cur):
+            if i == n:
+                ans.append(cur[:])
                 return
 
-            noTake = backtracking(i, j+1, cur)
-            
-            curTake = s[i:j+1]
-            if curTake == curTake[::-1]:
-                cur.append(curTake)
-                take = backtracking(j+1, j+1, cur)
-                cur.pop()
+            for j in range(i, n):
+                curStr = s[i:j+1]
 
-            return 
+                if curStr == curStr[::-1]:
+                    cur.append(curStr)
+                    backtracking(j + 1, cur)
+                    cur.pop()
 
-        backtracking(0,0,[])
+        backtracking(0, [])
         return ans
-        

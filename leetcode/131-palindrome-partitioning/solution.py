d917d11ccd62c5097cda3ce112f9a50e9ef337e9
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-18 17:18:55 -0700

    Add solution: 131-palindrome-partitioning - solution

--- leetcode/131-palindrome-partitioning/solution.py
+++ leetcode/131-palindrome-partitioning/solution.py
@@ -1,20 +1,34 @@
 class Solution:
     def partition(self, s: str) -> list[list[str]]:
         n = len(s)
+
+        # pal[l][r] = s[l:r+1] is a palindrome
+        pal = [[False] * n for _ in range(n)]
+
+        for i in range(n):
+            pal[i][i] = True
+
+        for length in range(2, n + 1):
+            for l in range(n - length + 1):
+                r = l + length - 1
+
+                if s[l] == s[r] and (length == 2 or pal[l + 1][r - 1]):
+                    pal[l][r] = True
+
         ans = []
 
-        def backtracking(i, cur):
+        def dfs(i):
             if i == n:
                 ans.append(cur[:])
                 return
 
-            for j in range(i, n):
-                curStr = s[i:j+1]
-
-                if curStr == curStr[::-1]:
-                    cur.append(curStr)
-                    backtracking(j + 1, cur)
+            for r in range(i, n):
+                if pal[i][r]:
+                    cur.append(s[i:r + 1])
+                    dfs(r + 1)
                     cur.pop()
 
-        backtracking(0, [])
+        cur = []
+        dfs(0)
+
         return ans

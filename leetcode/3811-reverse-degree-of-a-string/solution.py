c166d297d2b08ceef5d1b5ec2cb826b54349a735
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-19 21:19:57 -0700

    Add solution: 3811-reverse-degree-of-a-string - solution

--- leetcode/3811-reverse-degree-of-a-string/solution.py
+++ leetcode/3811-reverse-degree-of-a-string/solution.py
@@ -1,8 +1,7 @@
 class Solution:
     def reverseDegree(self, s: str) -> int:
         total = 0
-        for i in range(1, len(s) + 1):
-            letter = s[i-1]
+        for i, letter in enumerate(s, start=1):
             positionVal = abs(ord('z') + 1 - ord(letter))
             total += (positionVal * i)
 

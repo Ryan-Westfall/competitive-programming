f5fbf58e7ebc4ca3cc9a61c8d6c6ecd131f737c5
Ryan <42101482+Ryan-Westfall@users.noreply.github.com>
2026-09-22 11:31:55 -0700

    Add solution: 1051-shortest-way-to-form-string - solution

--- leetcode/1051-shortest-way-to-form-string/solution.py
+++ leetcode/1051-shortest-way-to-form-string/solution.py
@@ -1,21 +1,34 @@
+from functools import cache
+
 class Solution:
     def shortestWay(self, source: str, target: str) -> int:
-        targetI = 0
-        prevI = -1
-        ans = 0
-        while targetI < len(target):
-            if targetI == prevI:
+        n = len(source)
+
+        @cache
+        def dp(i, c):
+            if i == n:
                 return -1
 
-            prevI = targetI
+            if source[i] == c:
+                return i
 
-            for c in source:
-                if targetI < len(target) and target[targetI] == c:
-                    targetI += 1
+            return dp(i + 1, c)
 
-            ans += 1
+        ans = 1
+        i = 0
+
+        for c in target:
+            pos = dp(i, c)
+
+            if pos == -1:
+                if dp(0, c) == -1:
+                    return -1
+
+                ans += 1
+                i = dp(0, c)
+            else:
+                i = pos
+
+            i += 1
 
         return ans
-
-
-        
